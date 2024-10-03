b9c2739f2a57c2be3b30bc47f7f678d068ebc6ed
juliancr14 <juliancardonarojas3@gmail.com>
2024-10-03 17:54:51 -0500

    Adding error 404 if endpoint dont exist

--- src/index.js
+++ src/index.js
@@ -11,6 +11,13 @@ app.use(express.json());
 app.use(indexRoutes);
 app.use("/api", employeesRoutes);
 
+//If endpoint dont exist
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: "Endpoint not found"
+    });
+});
+
 //Server config
 app.listen(3000);
 console.log("Server on port 3000");
