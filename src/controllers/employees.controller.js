60041fe0046d89912d54259b9124a11f5fcb5f68
juliancr14 <juliancardonarojas3@gmail.com>
2024-10-02 20:30:28 -0500

    Changing the form of show post object

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -9,7 +9,11 @@ export const postEmployee = async (req, res) => {
     const { name, salary } = req.body;
 
     const [rows] = await pool.query(`INSERT INTO employee (name, salary) VALUES (?, ?)`, [name, salary]);
-    res.send({ rows })
+    res.send({
+        id: rows.insertId,
+        name,
+        salary
+    })
 };
 
 export const putEmployee = (req, res) => res.send("Actualizando empleado");
