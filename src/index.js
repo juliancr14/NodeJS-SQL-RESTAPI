108dcc4b7ce06acac5d88fd1fa698c5fc12f621c
juliancr14 <juliancardonarojas3@gmail.com>
2024-10-03 19:01:43 -0500

    Change order to proyect architecture

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
-import express from "express";
-import employeesRoutes from "./routes/employees.routes.js";
-import indexRoutes from "./routes/index.routes.js";
+import app from "./app.js";
 import { PORT } from "./config.js";
-
-const app = express();
-
-//Middleware
-app.use(express.json());
-
-//Using endpoints routes
-app.use(indexRoutes);
-app.use("/api", employeesRoutes);
-
-//If endpoint dont exist
-app.use((req, res, next) => {
-    res.status(404).json({
-        message: "Endpoint not found"
-    });
-});
 
 //Server config
 app.listen(PORT);
